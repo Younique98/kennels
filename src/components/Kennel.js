9da185034d60e4007edc2bf197c4221e8e45eb1c
Erica Thompson <younique98@gmail.com>
2021-02-25 12:51:03 -0800

    added new Componenets to store the Nav Bar and the ApplicationVIews

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,24 +1,11 @@
 import React from "react"
-import { AnimalCard } from "./animal/AnimalCard"
-import "./animal/Animal.css"
-import {PropsAndState} from "./PropsAndState.js"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
+import "./Kennel.css"
 
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <PropsAndState yourName={"Erica"} />
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
+        <NavBar />
+        <ApplicationViews />
     </>
 )
