0ab27239ced0950b24273399010e57cf6771b28f
Erica Thompson <younique98@gmail.com>
2021-02-25 13:43:14 -0800

    fixed formating in animal list and also the two warnings that popped up in the console.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,20 +1,24 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import { Home } from "../Home"
-import { AnimalCard } from "./animal/AnimalCard"
+import React from "react";
+import { Route } from "react-router-dom";
+import { Home } from "../Home";
+
+import { AnimalProvider } from "./animal/AnimalProvider";
+import { AnimalList } from "./animal/AnimalList"
 
 export const ApplicationViews = () => {
-    return (
-        <>
-            {/* Render the location list when http://localhost:3000/ */}
-            <Route exact path="/">
-                <Home />
-            </Route>
+  return (
+    <>
+      {/* Render the location list when http://localhost:3000/ */}
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-            {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-            </Route>
-        </>
-    )
-}
+      {/* Render the animal list when http://localhost:3000/animals */}
+      <AnimalProvider>
+        <Route exact path="/animals">
+          <AnimalList />
+        </Route>
+      </AnimalProvider>
+    </>
+  );
+};
