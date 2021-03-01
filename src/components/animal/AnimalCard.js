614a8f80166ad716f381207f8a70774f727510af
Erica Thompson <younique98@gmail.com>
2021-03-01 11:32:06 -0800

    created components for animals, customer, location and employees

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -2,9 +2,11 @@ import React from "react"
 import "./Animal.css"
 
 
-export const AnimalCard = ({ animal }) => (
+export const AnimalCard = ({ animalProp }) => (
     <section className="animal">
-        <h3 className="animal__name">{animal.name}</h3>
-        <address className="location__address">{animal.location.name}</address>
+        <h3 className="animal__name">{animalProp.name}</h3>
+        <address className="animal_breed">{animalProp.location.name}</address>
+        <address className="animal_owner">Owner: {animalProp.customers.name}</address>
+        <address className="animal_location">{animalProp.location.name}</address>
     </section>
 )
---
+++ src/components/locations/LocationCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Location.css"
+
+
+export const LocationCard = ({locationProps}) => (
+    <section className="location">
+        {/* {console.log(locationProps)} */}
+        <h3 className ="location__name">Name: {locationProps.name}</h3>
+        <div clasName="location__address">Address: {locationProps.address}</div>
+    </section>
+)
