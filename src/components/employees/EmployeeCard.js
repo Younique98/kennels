4af4a3fc31a94d210c5ea6bf05fc7af2dfffc3ca
Erica Thompson <younique98@gmail.com>
2021-03-02 11:32:48 -0800

    rendered employees to the dom utilizing the structure of employeecard

--- src/components/employees/EmployeeCard.js
+++ src/components/employees/EmployeeCard.js
@@ -5,8 +5,6 @@ export const EmployeeCard = ({employeeProps}) => (
     <section className="employee">
         {/* {console.log(employeeProps)} */}
         <h3 className ="employee__name">Name: {employeeProps.name}</h3>
-        <div className="employee__location">Location: {employeeProps.locations.name}</div>
-        <div className="employee__location">Location: {employeeProps.locations.address}</div>
     </section>
 )
 
--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -8,23 +8,22 @@ import { LocationContext } from "../locations/LocationProvider"
 export const EmployeeList = () => {
   // This state changes when `getEmployees()` is invoked below
   const { employees, getEmployees } = useContext(EmployeeContext);
-  const { locations, getLocations } = useContext(LocationContext)
+  
   //useEffect - reach out to the world for something
   useEffect(() => {
     console.log("EmployeeList: useEffect - getEmployees");
     getEmployees()
-    .then(getLocations)
   }, []);
 
   return (
     <>
       <div className="employees">
         {console.log("EmployeeList: Render", employees)}
-        {console.log("EmployeeList: Render", locations)}
+       
         {employees.map((employee) => {
-          const located = locations.find(location => location.id === employee.locationId)
-          return <EmployeeCard key={employee.id} employeeProps={employee} located={located}/>;
-        })}
+          return <EmployeeCard key={employee.id} employeeProps={employee}/>;
+        })
+        }
       </div>
     </>
   );
